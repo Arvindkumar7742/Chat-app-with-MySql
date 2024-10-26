31af052fb0510f59c71c9086e572424a5582c120
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2024-10-26 14:23:23 +0530

    initted socket connections for users

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,56 @@
-const express= require('express');
-const mysql = require('mysql');
+const express = require('express');
 const cors = require('cors');
-const {db} = require("./config/dbConnect")
+const { db } = require("./config/dbConnect")
+const http=require("http");
 const app = express();
+const {Server}=require("socket.io");
+require('dotenv').config();
+
+//middlewares
+app.use(express.json());
 app.use(cors());
 
-app.get("/",(req,res)=>{
+//Home route
+app.get("/", (req, res) => {
     return res.json("You are on backend");
 })
 
-app.get('/users',(req,res)=>{
+//dummy route to fetch data
+app.get('/students', (req, res) => {
     const sql = 'SELECT * FROM students';
-    db.query(sql,(err,data)=>{
-        if(err){
-            console.log("error while the executing the query",err);
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.log("error while the executing the query", err);
             return res.json(err);
         }
         return res.json(data);
     })
 })
 
-app.listen('8080',()=>{
-    console.log("app is listing on port no 8080");
+//To add the socket connections
+const server=http.createServer(app);
+app.listen(process.env.PORT, () => {
+    console.log(`app is listing on port no ${process.env.PORT}`);
+})
+
+const io=new Server(server,{
+    cors:{
+      origin:process.env.ORIGIN_URL,
+      methods:["GET","POST"]
+    }
+});
+
+
+io.on("connection",(socket)=>{
+
+      socket.on("join-room",(roomid)=>{
+           socket.join(roomid);
+      })
+
+      socket.on("send-msg",(message,currentRoomid)=>{
+          socket.to(currentRoomid).emit("receive-msg",message);
+      });
+      socket.on("disconnect",(roomid)=>{
+         socket.leave(roomid);
+      });
 })
